cd60d1cb5692d7e8b96481da7c410bea642c5954
Denis <makhonkodenis@gmail.com>
2019-03-14 12:17:02 +0200

    ADD: new checks

---
+++ checks/get_text_functions.php
@@ -0,0 +1,50 @@
+<?php
+class GetTextFunctions implements themecheck {
+
+	protected $error = array();
+
+	function check( $php_files, $css_files, $other_files ) {
+
+		$ret = true;
+
+		$checks = array(
+			'/\W_[_e]\(\s*(\'|\")[\w\s]+/' => __( 'Use only <strong>esc_html__</strong> or <strong>esc_html_e</strong> translation function', 'theme-check' )
+			);
+
+		foreach ( $php_files as $php_key => $phpfile ) {
+
+			if ( false !== strpos( $php_key, 'class-tgm-plugin-activation' ) ) {
+				continue;
+			}
+
+			foreach ( $checks as $key => $check ) {
+				checkcount();
+				if ( preg_match_all( $key, $phpfile, $matches ) ) {
+					$this->add_errors( $matches, $check, $php_key );
+				}
+			}
+		}
+		return $ret;
+	}
+
+	/**
+	 * Adds all breadcrums errors
+	 */
+	function add_errors( $matches, $check, $php_key ) {
+
+		if ( empty( $matches[0] ) ) {
+			return;
+		}
+
+		foreach ( $matches[0] as $match ) {
+			$error = $match;
+			$grep = tc_grep( $error, $php_key );
+			$this->error[] = sprintf('<span class="tc-lead tc-warning">'.__('WARNING','theme-check').'</span>: '.__('%1$s was found in the file %2$s %3$s.%4$s', 'theme-check'), '<strong>' . $error . '</strong>', '<strong>' . $php_key . '</strong>', $check, $grep ) ;
+		}
+
+	}
+
+	function getError() { return $this->error; }
+}
+
+$themechecks[] = new GetTextFunctions;
---
+++ checks/get_text_usage.php
@@ -0,0 +1,52 @@
+<?php
+class GetTextUsage implements themecheck {
+
+	protected $error = array();
+
+	function check( $php_files, $css_files, $other_files ) {
+
+		global $data, $themename;
+
+		$theme_data = wp_get_theme( $themename );
+		$tex_domain = $theme_data->get( 'TextDomain' );
+
+		$ret = true;
+
+		if ( ! $tex_domain ) {
+			return $ret;
+		}
+
+		$prefix = str_replace( '-', '_', strtolower( $tex_domain ) );
+
+		$checks = array(
+			'/_[_e]\([\s]?[\'\"](\<\w|[a-zA-Z \!\?\,\.]*\<[\w])/' => __( 'HTML not allowed in translation functions.', 'theme-check' ),
+			'/_[_e]\([\s]?[\'\"](\$\w|[a-zA-Z \?\!\.\,]*\$\w)/' => __( 'Variables not allowed in translation functions.', 'theme-check' ),
+			'/_[_e]\([\s]?[\'\"].*?[\'\"]\,[\s]?[a-zA-Z\$]*[\s]?\)/' => __( 'Variables and constants not allowed as textdomain.', 'theme-check' ),
+		);
+
+		foreach ( $php_files as $php_key => $phpfile ) {
+
+			if ( false !== strpos( $php_key, 'class-tgm-plugin-activation' ) ) {
+				continue;
+			}
+
+			foreach ( $checks as $key => $check ) {
+				checkcount();
+				if ( preg_match( $key, $phpfile, $matches ) ) {
+					$error = ltrim( rtrim( $matches[0], '(' ) );
+					$grep = tc_grep( $error, $php_key );
+					$this->error[] = sprintf(
+						'<span class="tc-lead tc-warning">' . __( 'WARNING','theme-check' ) . '</span>: ' . __( 'Maybe wrong translation function usage in %1$s. %2$s', 'theme-check' ),
+						'<strong>' . $php_key . '</strong>',
+						$grep
+					);
+				}
+			}
+		}
+		return $ret;
+	}
+
+	function getError() { return $this->error; }
+}
+
+$themechecks[] = new GetTextUsage;
